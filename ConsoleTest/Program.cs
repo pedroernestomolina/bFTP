a6902830a46bb7abd2072584638ebdbcda36a4ff
ernesto_foxsystem@hotmail.com <ernesto_foxsystem@hotmail.com>
2022-07-18 14:52:19 -0400

    Actualizacion

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -10,18 +10,24 @@ namespace ConsoleTest
     {
         static void Main(string[] args)
         {
-            //IPosOffLine.IProvider _test = new ProvPosOffLine.Provider("");
-            //_test.setServidorRemoto("localhost", "00000001");
+            IPosOffLine.IProvider _test = new ProvPosOffLine.Provider("");
+            _test.setServidorRemoto("localhost", "00000001");
 
-            //var _fichaDto = new DtoLibPosOffLine.Servidor.MonitorCambiosBD.SubirHost.Insertar.Ficha()
-            //{
-            //    fecha = DateTime.Now.Date,
-            //    descripcion = "PARA LA ENUMERACION DE LOS CIERRE DE VENTA",
-            //    cmd = @"ALTER TABLE `pos_arqueo`  ADD `codigo_sucursal` VARCHAR(10) NOT NULL AFTER `montoNcr`,  ADD `id_equipo` VARCHAR(2) NOT NULL AFTER `codigo_sucursal`,  ADD `cierre_numero` INT NOT NULL AFTER `id_equipo`",
-            //    aplicaPosOffLine = "1",
-            //    aplicaPosOnLine = "1",
-            //};
-            //_test.MonitorCambiosBD_SubirHost_InsertarCambio (_fichaDto);
+            var _fichaDto = new DtoLibPosOffLine.Servidor.MonitorCambiosBD.SubirHost.Insertar.Ficha()
+            {
+                fecha = DateTime.Now.Date,
+                descripcion = "PARA INVENTARIO: DEFINIR CAMPO VOLUMEN",
+                cmd = @"ALTER TABLE `productos`  ADD `volumen` DECIMAL(14,6) NOT NULL AFTER `pdf_pto`",
+                aplicaPosOffLine = "1",
+                aplicaPosOnLine = "1",
+            };
+            var rt1 = _test.MonitorCambiosBD_SubirHost_InsertarCambio(_fichaDto);
+            if (rt1.Result == DtoLib.Enumerados.EnumResult.isError)
+            {
+                Console.WriteLine("ERROR:");
+                Console.WriteLine(rt1.Mensaje);
+            }
+            Console.ReadKey();
         }
     }
 }
